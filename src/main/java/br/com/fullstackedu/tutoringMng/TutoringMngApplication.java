553ab8ff81f21b30a3edbd05b31356fb7bbe8210
Eder Sparenberger <edsparen@gmail.com>
2024-04-23 10:59:02 -0300

    initial commit with project infra

---
+++ src/main/java/br/com/fullstackedu/tutoringMng/TutoringMngApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstackedu.tutoringMng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringMngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullstackedu/tutoringMng/TutoringMngApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstackedu.tutoringMng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoringMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
