7da9f5467ac841c34348bd65738692ba75e1aa2a
Eder Sparenberger <edsparen@gmail.com>
2024-04-23 17:44:17 -0300

    fix(infra): adds spring security settings

---
+++ src/main/java/br/com/fullstackedu/tutoringMng/infra/SecurityConfig.java
@@ -0,0 +1,88 @@
+package br.com.fullstackedu.tutoringMng.infra;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${jwt.public.key}")
+    RSAPublicKey key;
+
+    @Value("${jwt.private.key}")
+    RSAPrivateKey priv;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/alunos").permitAll()
+                        .requestMatchers("/tutores").permitAll()
+                        .requestMatchers("/agendas").permitAll()
+                        .requestMatchers("/materiais").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated() // Require authentication for other endpoints
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oath2 ->
+                        oath2.jwt(Customizer.withDefaults()) // configurar o JWT com os padrões de projeto -> os beans JwtDecoder e JwtEncoder
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .sessionManagement(session ->
+                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+        ;
+        return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder
+                .withPublicKey(this.key)
+                .build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+
+        JWK jwk = new RSAKey.Builder(this.key)
+                .privateKey(this.priv)
+                .build();
+
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>( // lista imutável de JWK
+                new JWKSet(jwk)
+        );
+
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
