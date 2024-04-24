101cb9aacb28edf36be3aa794ec473399d6c7d4a
Eder Sparenberger <edsparen@gmail.com>
2024-04-24 11:22:28 -0300

    feat(agenda): adds GET route to return future schedules by aluno-id and tutor-id

--- src/main/java/br/com/fullstackedu/tutoringMng/database/repository/AgendaRepository.java
+++ src/main/java/br/com/fullstackedu/tutoringMng/database/repository/AgendaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AgendaRepository extends JpaRepository<AgendaEntity, Long> {
@@ -14,4 +15,9 @@ public interface AgendaRepository extends JpaRepository<AgendaEntity, Long> {
     @Query("SELECT a FROM AgendaEntity a WHERE a.aluno.id = :alunoId ORDER BY a.data ASC")
     List<AgendaEntity> findAllByAlunoIdOrderByDataAsc(@Param("alunoId") Long alunoId);
 
+    @Query("SELECT a FROM AgendaEntity a WHERE a.aluno.id = :alunoId AND a.data >= :currentDate")
+    List<AgendaEntity> findFutureAgendasByAlunoId(@Param("alunoId") Long alunoId, @Param("currentDate") LocalDate currentDate);
+
+    @Query("SELECT a FROM AgendaEntity a WHERE a.tutor.id = :tutorId AND a.data >= :currentDate")
+    List<AgendaEntity> findFutureAgendasByTutorId(@Param("tutorId") Long tutorId, @Param("currentDate") LocalDate currentDate);
 }
